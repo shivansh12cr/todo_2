aecdc66d3c85c2d99b19906bdbf2a43a1c61f85b
shivansh12cr <164125128+shivansh12cr@users.noreply.github.com>
2025-04-18 18:15:15 +0530

    Update index.js
    some changes

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use("/api/v2",list);
 
 
 
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
     app.use(express.static(path.resolve(__dirname, "frontend", "dist")));
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 });
